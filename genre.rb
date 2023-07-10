2dc1671675338a980af6d465105361acfe81684b
Cristian Zambrano <cristian@zamcham.com>
2023-07-10 15:44:52 -0400

    Update genre.rb
    Add initialiser and methods

--- genre.rb
+++ genre.rb
@@ -1,1 +1,14 @@
+class Genre
+  attr_reader :id, :name, :items
+
+  def initialize(id, name)
+    @id = id || Random.rand(1..10_000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
 
