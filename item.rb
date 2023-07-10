4f9af17bdb3b074624a01ae6e077480c0d2b1deb
Naan Ahmed Mohammed <124832435+naanmohammed@users.noreply.github.com>
2023-07-10 22:45:02 +0300

    Initialize class properties in item.rb

--- item.rb
+++ item.rb
@@ -1,1 +1,15 @@
+require 'date'
 
+class Item
+  attr_accessor :archived
+
+  def initialize(genre, author, source, label, publish_date)
+      @id = Random.rand(1...1000)
+      @genre = genre
+      @author = author
+      @source = source
+      @label = label
+      @publish_date = Date.parse(publish_date)
+      @archived = false
+  end
+end
