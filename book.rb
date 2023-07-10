c8f475f3b902471f88321bdece1d477658c2c864
Cristian Zambrano <cristian@zamcham.com>
2023-07-10 19:11:59 -0400

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,1 +1,16 @@
+require-relatice 'Item'
 
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(id, genre, author, source, label, publish_date, archived, publisher, cover_state)
+    super(id, genre, author, source, label, publish_date, archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    # Implement the logic for determining if the book can be archived
+    # Return true or false based on the conditions
+  end
+end
