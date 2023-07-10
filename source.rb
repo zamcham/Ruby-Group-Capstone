7acaa711e57ce109152e311a21915ede517a9b74
Naan Ahmed Mohammed <124832435+naanmohammed@users.noreply.github.com>
2023-07-10 23:09:29 +0300

    Update source.rb

--- source.rb
+++ source.rb
@@ -1,1 +1,14 @@
+class Source
+  attr_reader :id, :name, :items
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
 
