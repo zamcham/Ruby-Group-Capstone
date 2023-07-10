168b2265c9b70ba8bfac967be85890f61d04ba3b
Naan Ahmed Mohammed <124832435+naanmohammed@users.noreply.github.com>
2023-07-10 23:24:02 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,15 +3,14 @@ class Main
     loop do
       show_console_options
       option = get_user_input('Enter your choice: ').to_i
-      break if option == 13
+      break if option == 10
       select_option(option)
     end
   end
 
   def show_console_options
-    options_array = ['List all books', 'List all music albums', 'List all movies', 'List all games', 'List all genres',
-                     'List all labels', 'List all authors', 'List all sources', 'Add book', 'Add a music album', 'Add movie',
-                     'Add a game', 'Exit']
+    options_array = ['List All Books', 'List All Music Albums', 'List All Games', 'List All Genres',
+                     'List All Labels', 'List All Authors', 'Add Book', 'Add a Music Album', 'Add a Game', 'Exit']
     puts "\nWelcome to the Ruby Console App!"
     puts "\nPlease choose an option from the list below:"
     puts '-------------------------------------------'
@@ -22,8 +21,11 @@ class Main
 
   def select_option(option)
     case option
-    when 1..12
+    when 1..6
       puts "Method #{option}"
+      sleep(1)
+    when 7..9
+      puts "Method #{option + 1}"
       sleep(1)
     else
       puts '-------------------------------------------'
