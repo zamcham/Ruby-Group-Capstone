e52b64069310ea269da139a107c82510728239fb
oovillagran <oscar.villagran@hotmail.com>
2023-07-10 16:57:13 -0500

    Add MusicAlbum class

--- music_album.rb
+++ music_album.rb
@@ -1,1 +1,11 @@
+require_relative 'item'
 
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(id, genre, author, publish_date, on_spotify)
+  end
+
+  def can_be_archived?
+  end
+end
