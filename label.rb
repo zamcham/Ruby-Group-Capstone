839b1cdda3d853ed8b46f35862699c94cb9b2460
Naan Ahmed Mohammed <124832435+naanmohammed@users.noreply.github.com>
2023-07-10 23:04:24 +0300

    Update label.rb

--- label.rb
+++ label.rb
@@ -1,1 +1,17 @@
+require_relative 'item'
 
+class Label
+  attr_reader :title, :color, :items, :id
+
+  def initialize(title, color, id = nil)
+    @id = id || Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_label(self)
+  end
+end
