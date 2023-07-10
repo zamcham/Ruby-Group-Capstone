b68500fed7789667eec791c24ef58853d651de41
Cristian Zambrano <cristian@zamcham.com>
2023-07-10 16:02:18 -0400

    Update author.rb

--- author.rb
+++ author.rb
@@ -1,1 +1,16 @@
+require-relative 'item'
 
+class Author
+  attr_accessor :id, :first_name, :last_name, :items
+
+  def initialize(id, first_name, last_name)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
