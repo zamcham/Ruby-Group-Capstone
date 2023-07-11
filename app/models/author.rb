7b514605e81111b2ee337a861243cf34d7cd331d
naanmohammed <naanahmed24@gmail.com>
2023-07-11 17:02:07 +0300

    Add author class

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,17 +1,19 @@
 require_relative 'item'
 
-# Represents Book Author class.
 class Author
-  attr_accessor :id, :first_name, :last_name, :items
+  attr_reader :id, :first_name, :last_name, :items
 
   def initialize(id, first_name, last_name)
-    @id = id
+    @id = id || Random.rand(1...1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
   def add_item(item)
+    raise TypeError, 'Invalid type, must be an Item instance' unless item.is_a?(Item)
+
     @items << item
+    item.author = self
   end
 end
