191d1f8529817bf96b11659179e58aafeb0384aa
oovillagran <oscar.villagran@hotmail.com>
2023-07-10 22:39:16 -0500

    Fix linters

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'item'
 require_relative 'music_album'
 require_relative 'genre'
--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'app'
 
 # Represents the main entry point for the Ruby Console App.
